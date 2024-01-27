b3fd4a2511347a13cd6d4bdef9e7384562d9026e
akanshavrma <134785932+akansha2222@users.noreply.github.com>
2024-01-27 17:46:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 // complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	greet = function()
+	{
+			return `Hello, my name is $name. I am $age years old`
+	}
+}
 
-function Employee(name, age, jobTitle) {}
+function Employee(name, age, jobTitle) {
+	Car.call(this, make, model);
+	this.jobTitle = jobTitle
+	
+}
+Employee.prototype = Object.create(Person.prototype)}
 
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
+Employee.prototype.jobGreet = function() {
+	return this.jobTitle
+}
